8b44be6b5d7d6e6115d5bef82ed307d176bca641
Wuqly <narseniy9@yandex.ru>
2023-10-08 23:20:24 +0300

    add comment

--- test.js
+++ test.js
@@ -1,34 +1,30 @@
-var otvet = document.getElementById("otvet").hidden = false;
-var proveritNextq = document.getElementById("proverit-nextq").style.gridTemplateColumns = "2fr auto auto";
-
-
+var proveritNextq = document.getElementById("proverit-nextq").style.gridTemplateColumns = "2fr auto auto"; // Выравниваем конпки "Проверить ответ" и "Ответ"
 
 function proverit(num){
-    otv_uch_1 = document.getElementById('o_1').value;
-    var array = ["Main"]
-    var num_1 = 0; // Main
-    if (array[num] == otv_uch_1){
-        document.getElementById('rezultat').innerHTML = "Задание выполнено верно";
-        document.getElementById("rezultat").style.color = '#1ac91a';
-        document.getElementById("proverit-nextq").style.gridTemplateColumns = "10.5rem 18rem 0 auto";
-        document.getElementById("nextq").style.display="none";
+    otv_uch_1 = document.getElementById('otvet_uch').value; // Проверяем что ввел ученик
+    var array = ["Main"] 
+    var num_1 = 0; // Main // Индекс подставляем в onclick
+    if (array[num] == otv_uch_1){ // Если ответ ученика верный выводим "Задание выполнено верно"
+        document.getElementById('rezultat').innerHTML = "Задание выполнено верно"; // Вывод сообщения
+        document.getElementById("rezultat").style.color = '#1ac91a'; // меняем цвет сообщения на зеленый
+        document.getElementById("proverit-nextq").style.gridTemplateColumns = "10.5rem 18rem 0 auto"; // Подравниваем кнопки
         $('.hide-otvet').fadeOut(0).fadeIn(100);
-        $('.hide-otvet').hide();
+        $('.hide-otvet').hide(); // Скрываем кнопку "Ответ"
         $('.rezultat').fadeOut(0).fadeIn(100);
         $('.hide').fadeOut(0).fadeIn(100);
     }
-    else{
-        document.getElementById('rezultat').innerHTML = "Задание выполнено неверно, попробуйте еще раз";
-        document.getElementById("rezultat").style.color = '#c91a1a';
-        document.getElementById("nextq").style.display = "none";
-        document.getElementById("proverit-nextq").style.gridTemplateColumns = "10.5rem auto 10.7rem";
-        $('.rezultat').fadeOut(0).fadeIn(100);
+    else{ // Если ответ ученика неверный выводим "Задание выполнено неверно, попробуйте еще раз"
+        document.getElementById('rezultat').innerHTML = "Задание выполнено неверно, попробуйте еще раз"; // Вывод сообщения
+        document.getElementById("rezultat").style.color = '#c91a1a'; // Меняем цвет сообщения на красный
+        document.getElementById("nextq").style.display = "none"; // Не даем появится кнопке "Следующее задание"
+        document.getElementById("proverit-nextq").style.gridTemplateColumns = "10.5rem auto 10.7rem"; // Подравниваем кнопки
+        $('.rezultat').fadeOut(0).fadeIn(100); // Плавное появление сообщения и кнопки
         $('.hide-otvet').fadeOut(0).fadeIn(100);
         $('.hide-otvet').show();
     }
 }
 
-var clickToHide2 = document.querySelector('#otvet');
+var clickToHide2 = document.querySelector('#otvet'); // Здесь показываем/скрываем кнопки "Ответ" и "Скрыть" 
 	clickToHide2.addEventListener("click", hideVisibleElem);
 	function hideVisibleElem() {
 	let wpcraftBox1 = document.querySelector('.otvet-z');
